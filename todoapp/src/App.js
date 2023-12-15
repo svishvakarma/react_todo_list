d8ffa7c09fabc8f83761512e75204b341661110e
svishvakarma <svishvakarma@bestpeers.com>
2023-12-15 17:24:36 +0530

    Used useState hook

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <>
     <div className="App">
-     
+     <BrowserRouter>
+     <Routes>
+      <Route path='/home' element={<Home />}> </Route>
+     </Routes>
+     </BrowserRouter>
     </div>
+    </>
   );
 }
 
--- todoapp/src/components/Home.js
+++ todoapp/src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Home() {
+    const [data, setData] = useState('')
+    const UpdateData=()=>{
+        setData("Home component")
+    }
   return (
     <div>
-      
+      <button onClick={UpdateData}>
+        Get data
+      </button>
+      <p>{data}</p>
     </div>
   )
 }
