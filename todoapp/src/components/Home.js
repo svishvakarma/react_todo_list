62d494b4285cca6a803fb89a85e35eefb669696e
svishvakarma <svishvakarma@bestpeers.com>
2023-12-15 18:06:20 +0530

    created Data

--- todoapp/src/components/Home.js
+++ todoapp/src/components/Home.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Home() {
-    const [data, setData] = useState('')
-    const UpdateData=()=>{
-        setData("Home component")
-    }
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addData = () => {
+    setData((prevData) => [...prevData, inputValue]);
+    setInputValue(''); 
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addData();
+  };
+
   return (
     <div>
-      <button onClick={UpdateData}>
-        Get data
-      </button>
-      <p>{data}</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Data:
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">
+          Add Data
+        </button>
+      </form>
+      <ol>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ol>
     </div>
-  )
+  );
 }
